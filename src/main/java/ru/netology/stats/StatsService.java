62607d810b515967e8d61eb5c804f63e1a8b75b9
lili tolmacheva <tolmachevy2003@mail.ru>
2023-06-08 21:35:28 +0300

    Gitignore added

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,54 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public long sum(long[] sales) {
+        long totalSale = 0;
+        for (long sale : sales) {
+            totalSale += sale;
+        }
+        return totalSale;
+    }
+
+    public long average(long[] sales) {
+        long totalSale = 0;
+        for (long sale : sales) {
+            totalSale += sale;
+        }
+        long averageSale = totalSale / 12;
+        return averageSale;
+    }
+
+    public int monthMaximumSale(long[] sales) {
+        int monthMaximum = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[monthMaximum]) {
+                monthMaximum = i;
+            }
+        }
+        return monthMaximum + 1;
+    }
+
+    public int saleBellowAverage(long[] sales) {
+        int counter = 0;
+
+        for (long sale : sales) {
+            if (sale < average(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public int saleAboveAverage(long[] sales) {
+        int counter = 0;
+
+        for (long sale : sales) {
+            if (sale < average(sales)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+}
